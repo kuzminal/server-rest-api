ae12383a13569dd318c68baab3dc4457f564b649
Кузьмин Алексей <aleschakuzm1n@yandex.ru>
2023-03-01 11:39:58 +0300

    сделал миграции и добавил новую ручку в API

---
+++ config/rbr_srv_side/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Server(models.Model):
+    name = models.CharField('name', max_length=255)
+    ip_address = models.GenericIPAddressField('IP', max_length=16, default='0.0.0.0')
+    description = models.TextField('description', max_length=255, default='no_description')
+    server_is_active = models.BooleanField('server_is_active', max_length=5, default=False)
+
+    class Meta:
+        managed = True
+        verbose_name = 'Server'
---
+++ config/rbr_srv_side/views.py
@@ -0,0 +1,23 @@
+from rest_framework import generics
+from .serializer import ServerSerializer, ServerShortSerializer
+from .models import Server
+
+
+class ServerViewSet(generics.ListAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+
+
+class ServerAddView(generics.CreateAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+
+
+class ServerDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerSerializer
+
+
+class ServerShortViewSet(generics.ListAPIView):
+    queryset = Server.objects.all()
+    serializer_class = ServerShortSerializer
